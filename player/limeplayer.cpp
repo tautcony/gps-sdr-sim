edb8186cca13d26953d8a3f85b80d63758825bd9
TautCony <i@tautcony.xyz>
2023-09-26 10:05:06 +0800

    feat: improve logger in limeplayer

--- player/limeplayer.cpp
+++ player/limeplayer.cpp
@@ -6,6 +6,7 @@
 #include <csignal>
 #include <string>
 #include <algorithm>
+#include <spdlog/spdlog.h>
 
 #ifdef _WIN32
 #include "ya_getopt.h"
@@ -48,32 +49,6 @@
 #define STRINGIFY2(X) #X
 #define STRINGIFY(X) STRINGIFY2(X)
 
-#ifdef _WIN32
-#define NOMINMAX
-#include <windows.h>
-int gettimeofday(struct timeval* tp, struct timezone* tzp) {
-    // Note: some broken versions only have 8 trailing zero's, the correct epoch has 9 trailing zero's
-    // This magic number is the number of 100 nanosecond intervals since January 1, 1601 (UTC)
-    // until 00:00:00 January 1, 1970
-    static const uint64_t EPOCH = ((uint64_t)116444736000000000ULL);
-
-    SYSTEMTIME  system_time;
-    FILETIME    file_time;
-    uint64_t    time;
-
-    GetSystemTime(&system_time);
-    SystemTimeToFileTime(&system_time, &file_time);
-    time = ((uint64_t)file_time.dwLowDateTime);
-    time += ((uint64_t)file_time.dwHighDateTime) << 32;
-
-    tp->tv_sec = (long)((time - EPOCH) / 10000000L);
-    tp->tv_usec = (long)(system_time.wMilliseconds * 1000);
-    return 0;
-}
-#else
-#include <sys/time.h>
-#endif
-
 static int control_c_received = 0;
 #ifdef _WIN32
 BOOL WINAPI control_c_handler(DWORD fdwCtrlType)
@@ -104,6 +79,7 @@ static void print_usage(const char *program_name) {
             "\t" "-g, --gain <gain>       configure the so-called normalized RF gain in [0.0 .. 1.0] (default: 1.0 max RF power)" "\n"
             "\t" "-h, --help              print this help message" "\n"
             "\t" "-i, --index <index>     select specific LimeSDR device if multiple devices connected (default: 0)" "\n"
+            "\t" "-l, --log-level <level> configure log level in { 0(trace), 1(debug), 2(info), 3(warn), 4(err), 5(critical), 6(off) } (default: 2)" "\n"
             "\t" "-s, --samplerate <samplerate>" "\n"
             "\t" "                        configure sampling rate for TX channels (default: " STRINGIFY(TX_SAMPLERATE) ")" "\n"
         "Example:" "\n"
@@ -125,7 +101,6 @@ int error(int exit_code) {
     exit(exit_code);
 }
 
-
 // File contains interleaved signed 16-bit IQ values, either with only 12-bit data, or with 16-bit data
 typedef struct { int16_t i; int16_t q; } s16iq_sample_s;
 // File contains interleaved signed  8-bit IQ values
@@ -136,7 +111,7 @@ typedef int8_t iq_4_sample_s;
 int main(int argc, char *const argv[]) {
 #ifdef _WIN32
     if (!SetConsoleCtrlHandler(control_c_handler, TRUE)) {
-        printf("Could not set control handler.\n");
+        spdlog::error("Could not set control handler.");
     }
 #else
     struct sigaction control_c{};
@@ -161,6 +136,19 @@ int main(int argc, char *const argv[]) {
     }
 #endif
 
+    spdlog::set_pattern("[%Y/%m/%d %H:%M:%S %z] [%^%l%$] %v");
+    LMS_RegisterLogHandler([](int lvl, const char* msg) {
+        switch (lvl)
+        {
+            case LMS_LOG_CRITICAL: spdlog::critical(msg); break;
+            case LMS_LOG_ERROR: spdlog::error(msg); break;
+            case LMS_LOG_WARNING: spdlog::warn(msg); break;
+            case LMS_LOG_INFO: spdlog::info(msg); break;
+            case LMS_LOG_DEBUG: spdlog::debug(msg); break;
+            default: break;
+        }
+    });
+
     double gain = 1.0;
     int32_t antenna = DEFAULT_ANTENNA;
     int32_t channel = 0;
@@ -179,13 +167,15 @@ int main(int argc, char *const argv[]) {
         {"gain",       required_argument, nullptr, 'g'},
         {"help",       no_argument,       nullptr, 'h'},
         {"index",      required_argument, nullptr, 'i'},
+        {"log-level",  optional_argument, nullptr, 'l'},
         {"samplerate", required_argument, nullptr, 's'},
         {nullptr,      no_argument,       nullptr, '\0'}
     };
 
+    int rawLevel;
     while (true) {
         int option_index = 0;
-        int c = getopt_long(argc, argv, "a:b:c:d:f:g:hi:s:", long_options, &option_index);
+        int c = getopt_long(argc, argv, "a:b:c:d:f:g:hl:i:s:", long_options, &option_index);
         if (c == -1) break;
 
         char *endptr = nullptr;
@@ -195,16 +185,20 @@ int main(int argc, char *const argv[]) {
             case 'c': channel    = (int32_t) strtol(optarg, &endptr, 10); break;
             case 'd': dynamic    = (int32_t) strtol(optarg, &endptr, 10); break;
             case 'f': path       = std::string(optarg);                   break;
-            case 'g': gain       =           strtod(optarg, &endptr);     break;
-            case 'h': print_usage(argv[0]);                               break;
+            case 'g': gain       = strtod(optarg, &endptr);               break;
+            case 'h':              print_usage(argv[0]);                  break;
             case 'i': index      = (int32_t) strtol(optarg, &endptr, 10); break;
+            case 'l': rawLevel   = (int32_t) strtol(optarg, &endptr, 10); break;
             case 's': sampleRate =           strtod(optarg, &endptr);     break;
         }
         if (endptr != nullptr && *endptr != '\0') {
-            fprintf(stderr, "Failed to parse argument for option -%c => %s\n", c, optarg);
+            spdlog::critical("Failed to parse argument for option -{} => {}", c, optarg);
             exit(EXIT_CODE_INVALID_ARGUMENTS);
         }
     }
+
+    spdlog::level::level_enum logLevel = (spdlog::level::level_enum) std::min(std::max(0, rawLevel), (int)spdlog::level::off);
+    spdlog::set_level(logLevel);
 
     if (path.empty()) {
         input_stream = stdin;
@@ -212,21 +206,21 @@ int main(int argc, char *const argv[]) {
     } else {
         input_stream = fopen(path.c_str(), "rb");
         if (input_stream == nullptr) {
-            fprintf(stderr, "fopen() failed: %s\n", path.c_str());
+            spdlog::critical("Failed to open signal file: {}", path);
             error(EXIT_CODE_INVALID_ARGUMENTS);
         }
     }
 
     int device_count = LMS_GetDeviceList(nullptr);
     if (device_count < 1) {
-        printf("no device connected, exiting...\n");
+        spdlog::critical("no device connected");
         error(EXIT_CODE_NO_DEVICE);
     }
     auto *device_list = (lms_info_str_t *) malloc(sizeof(lms_info_str_t) * device_count);
     device_count = LMS_GetDeviceList(device_list);
     if (device_list != nullptr) {
         for (int i = 0; i < device_count; ++i) {
-            printf("device[%d/%d]=%s" "\n", i + 1, device_count, device_list[i]);
+            spdlog::info("device[{}/{}]={}", i + 1, device_count, device_list[i]);
         }
     }
 
@@ -235,14 +229,14 @@ int main(int argc, char *const argv[]) {
     if (index < 0 || index >= device_count) {
         index = 0;
     }
-    printf("Using device index %d [%s]" "\n", index, device_list[index]);
+    spdlog::info("Using device index {} [{}]", index, device_list[index]);
 
     // Normalized gain shall be in [0.0 .. 1.0]
     gain = std::min(std::max(0.0, gain), 1.0);
-    printf("Using normalized gain: %lf" "\n", gain);
+    spdlog::info("Using normalized gain: {}", gain);
 
     dynamic = std::min(std::max(0, dynamic), MAX_DYNAMIC);
-    printf("Using normalized dynamic: %d" "\n", dynamic);
+    spdlog::info("Using normalized dynamic: {}", dynamic);
 
     if (LMS_Open(&device, device_list[index], nullptr)) {
         error(EXIT_CODE_LMS_INIT);
@@ -250,31 +244,31 @@ int main(int argc, char *const argv[]) {
     free(device_list);
 
     if (LMS_Reset(device)) {
-        printf("lmsReset (%s)" "\n", LMS_GetLastErrorMessage());
+        spdlog::critical("LMS_Reset failed");
         error(EXIT_CODE_LMS_INIT);
     }
     if (LMS_Init(device)) {
-        printf("lmsInit (%s)" "\n", LMS_GetLastErrorMessage());
+        spdlog::critical("LMS_Init failed");
         error(EXIT_CODE_LMS_INIT);
     }
 
     int channel_count = LMS_GetNumChannels(device, LMS_CH_TX);
     if (channel_count < 0) {
-        printf("LMS_GetNumChannels (%s)" "\n", LMS_GetLastErrorMessage());
+        spdlog::critical("LMS_GetNumChannels failed");
         error(EXIT_CODE_LMS_INIT);
     }
-    printf("Tx channel count: %d" "\n", channel_count);
+    spdlog::info("Tx channel count: {}", channel_count);
     if (channel < 0 || channel >= channel_count) {
         channel = 0;
     }
-    printf("Using channel: %d" "\n", channel);
+    spdlog::info("Using channel: {}", channel);
 
     int antenna_count = LMS_GetAntennaList(device, LMS_CH_TX, channel, nullptr);
     if (antenna_count < 0) {
-        printf("LMS_GetAntennaList (%s)" "\n", LMS_GetLastErrorMessage());
+        spdlog::critical("LMS_GetAntennaList failed");
         error(EXIT_CODE_LMS_INIT);
     }
-    printf("TX%d Channel has %d antenna(ae)" "\n", channel, antenna_count);
+    spdlog::info("TX{} Channel has {} antenna(ae)", channel, antenna_count);
     if (antenna < 0 || antenna >= antenna_count) {
         antenna = DEFAULT_ANTENNA;
     }
@@ -285,7 +279,7 @@ int main(int argc, char *const argv[]) {
         LMS_GetAntennaList(device, LMS_CH_TX, channel, antenna_name);
         for(int i = 0 ; i < antenna_count ; i++) {
             LMS_GetAntennaBW(device, LMS_CH_TX, channel, i, &antenna_bw);
-            printf("Channel %d, antenna [%s] has BW [%lf .. %lf] (step %lf)" "\n", channel, antenna_name[i], antenna_bw.min, antenna_bw.max, antenna_bw.step);
+            spdlog::info("Channel {}, antenna [{}] has BW [{} .. {}] (step {})", channel, antenna_name[i], antenna_bw.min, antenna_bw.max, antenna_bw.step);
             if (ANTENNA_AUTO >= antenna_count &&
                 ANTENNA_AUTO == antenna &&
                 antenna_bw.min < TX_FREQUENCY && TX_FREQUENCY < antenna_bw.max) {
@@ -293,7 +287,7 @@ int main(int argc, char *const argv[]) {
             }
         }
     }
-    printf("Using antenna %d: [%s]" "\n", antenna, antenna_name[antenna]);
+    spdlog::info("Using antenna {}: [{}]", antenna, antenna_name[antenna]);
     free(antenna_name);
 
     // LMS_SetAntenna(device, LMS_CH_TX, channel, antenna); // SetLOFrequency should take care of selecting the proper antenna
@@ -310,53 +304,53 @@ int main(int argc, char *const argv[]) {
     LMS_EnableChannel(device, LMS_CH_TX, channel, true);
 
     if (LMS_SetLOFrequency(device, LMS_CH_TX, channel, TX_FREQUENCY)) {
-        printf("setLOFrequency(%lf)=(%s)" "\n", TX_FREQUENCY, LMS_GetLastErrorMessage());
+        spdlog::critical("setLOFrequency({})=({})", TX_FREQUENCY, LMS_GetLastErrorMessage());
         error(EXIT_CODE_LMS_INIT);
     }
 
 #ifdef __USE_LPF__
     lms_range_t LPFBWRange{};
     LMS_GetLPFBWRange(device, LMS_CH_TX, &LPFBWRange);
-    printf("TX%d LPFBW [%lf .. %lf] (step %lf)" "\n", channel, LPFBWRange.min, LPFBWRange.max, LPFBWRange.step);
+    spdlog::info("TX{} LPFBW [{} .. {}] (step {})", channel, LPFBWRange.min, LPFBWRange.max, LPFBWRange.step);
     double LPFBW = TX_BANDWIDTH;
     if (LPFBW < LPFBWRange.min || LPFBW > LPFBWRange.max) {
         LPFBW = LPFBWRange.min;
     }
     if (LMS_SetLPFBW(device, LMS_CH_TX, channel, LPFBW)) {
-        printf("setLPFBW(%lf)=%d(%s)" "\n", LPFBW, LMS_GetLastErrorMessage());
+        spdlog::error("LMS_SetLPFBW({}) failed", LPFBW);
     }
     if (LMS_SetLPF(device, LMS_CH_TX, channel, true)) {
-        printf("enableLPF=(%s)" "\n", LMS_GetLastErrorMessage());
+        spdlog::error("LMS_SetLPF(true) failed");
     }
 #endif
 
     lms_range_t sampleRateRange{};
     if (LMS_GetSampleRateRange(device, LMS_CH_TX, &sampleRateRange)) {
-        printf("getSampleRateRange=(%s)" "\n", LMS_GetLastErrorMessage());
+        spdlog::error("LMS_GetSampleRateRange failed");
     } else {
-        printf("SampleRateRange: [%lf MHz .. %lf MHz] (step=%lf Hz)" "\n", sampleRateRange.min / 1e6, sampleRateRange.max / 1e6, sampleRateRange.step);
+        spdlog::info("SampleRateRange: [{} MHz .. {} MHz] (step={} Hz)", sampleRateRange.min / 1e6, sampleRateRange.max / 1e6, sampleRateRange.step);
     }
 
-    printf("Set sample rate to %lf Hz ..." "\n", sampleRate);
+    spdlog::info("Set sample rate to {} Hz ...", sampleRate);
     if (LMS_SetSampleRate(device, sampleRate, 0)) {
-        printf("setSampleRate=(%s)" "\n", LMS_GetLastErrorMessage());
+        spdlog::critical("LMS_SetSampleRate({}) failed", sampleRate);
         error(EXIT_CODE_LMS_INIT);
     }
     double actualHostSampleRate = 0.0;
     double actualRFSampleRate = 0.0;
     if (LMS_GetSampleRate(device, LMS_CH_TX, channel, &actualHostSampleRate, &actualRFSampleRate)) {
-        printf("getSampleRate=(%s)" "\n", LMS_GetLastErrorMessage());
+        spdlog::error("LMS_GetSampleRate failed");
     } else {
-        printf("actualRate %lf Hz (Host) / %lf Hz (RF)" "\n", actualHostSampleRate, actualRFSampleRate);
+        spdlog::info("actualRate {} Hz (Host) / {} Hz (RF)", actualHostSampleRate, actualRFSampleRate);
     }
 
-    printf("Calibrating ..." "\n");
+    spdlog::info("Calibrating ...");
     if (LMS_Calibrate(device, LMS_CH_TX, channel, TX_BANDWIDTH, 0)) {
-        printf("calibrate=(%s)" "\n", LMS_GetLastErrorMessage());
+        spdlog::critical("LMS_Calibrate failed");
         error(EXIT_CODE_LMS_INIT);
     }
 
-    printf("Setup TX stream ..." "\n");
+    spdlog::info("Setup TX stream ...");
     lms_stream_t tx_stream{};
     tx_stream.isTx = true;                         // TX channel
     tx_stream.channel = (uint32_t)channel;         // channel number
@@ -371,11 +365,11 @@ int main(int argc, char *const argv[]) {
     tx_meta.flushPartialPacket = false;            // send samples to HW after packet is completely filled
 
     if (LMS_SetupStream(device, &tx_stream)) {
-        printf("setupStream=(%s)" "\n", LMS_GetLastErrorMessage());
+        spdlog::critical("LMS_SetupStream failed");
         error(EXIT_CODE_LMS_INIT);
     }
     if (LMS_StartStream(&tx_stream)) {
-        printf("startStream=(%s)" "\n", LMS_GetLastErrorMessage());
+        spdlog::critical("LMS_StartStream failed");
         error(EXIT_CODE_LMS_INIT);
     }
 
@@ -391,24 +385,11 @@ int main(int argc, char *const argv[]) {
     int64_t loop_step = 0;
     auto print_progress = [&]() {
         if (0 != (loop_step++ % 100)) return;
-        struct timeval tv {};
-        char tm_buf[64];
-
-        gettimeofday(&tv, nullptr);
-        time_t current_time = tv.tv_sec;
-        auto current_tm = localtime(&current_time);
-        strftime(tm_buf, sizeof(tm_buf), "%Y-%m-%d %H:%M:%S", current_tm);
-
-#ifdef __APPLE__
-        printf("gettimeofday() => %s.%06d ; ", tm_buf, tv.tv_usec);
-#else
-        printf("gettimeofday() => %s.%06ld ; ", tm_buf, tv.tv_usec);
-#endif
         lms_stream_status_t status{};
         if (LMS_GetStreamStatus(&tx_stream, &status)) {
-
+            spdlog::error("LMS_GetStreamStatus failed");
         }
-        printf("TX rate: %lf MiB/s" "\n", status.linkRate / (1LL << 20));
+        spdlog::info("TX rate: {:06f} MiB/s", status.linkRate / (1LL << 20));
     };
 
     int16_t expand_lut[1 << 8][8] = {};
@@ -419,7 +400,7 @@ int main(int argc, char *const argv[]) {
     }
 
     double transmitBandwidth = sampleRate * (bits == 16 ? 16 : 12) * 2 / 8 / (1LL << 20);
-    printf("transmit bit mode: %d-bit, sample rate: %lf Hz, expected bandwidth: %lf MiB/s" "\n", bits, sampleRate, transmitBandwidth);
+    spdlog::info("transmit bit mode: {}-bit, sample rate: {} Hz, expected bandwidth: {} MiB/s", bits, sampleRate, transmitBandwidth);
 
     int sampleCount = 0;
     while (0 == control_c_received) {
@@ -451,14 +432,14 @@ int main(int argc, char *const argv[]) {
         print_progress();
         int sentSampleCount = LMS_SendStream(&tx_stream, sampleBuffer, sampleCount, nullptr, 1000);
         if (sentSampleCount < 0) {
-            printf("LMS_SendStream: (%s)" "\n", LMS_GetLastErrorMessage());
+            spdlog::error("LMS_SendStream failed");
         }
         tx_meta.timestamp += sentSampleCount;
     }
 
-    printf("Total transmit duration: %lfs\n", tx_meta.timestamp / sampleRate);
+    spdlog::info("Total transmit duration: {}s", tx_meta.timestamp / sampleRate);
 
-    printf("Releasing resources...\n");
+    spdlog::info("Releasing resources...");
     if (input_stream != stdin) {
         fclose(input_stream);
         input_stream = nullptr;
@@ -472,7 +453,7 @@ int main(int argc, char *const argv[]) {
     LMS_EnableChannel(device, LMS_CH_RX, channel, false);
     LMS_Close(device);
     device = nullptr;
-    printf("Done.\n");
+    spdlog::info("Done");
 
     if (control_c_received) {
         return (EXIT_CODE_CONTROL_C);
